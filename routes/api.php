bb593a689c2ba17ed62034a3df379b5a5a503d0f
CJ Batausa <caiusjayce.jaye@gmail.com>
2021-11-17 22:27:00 +0800

    first commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AirPlaneFlightControllerAPI;
+use App\Http\Controllers\API\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('test-query', [UsersController::class, 'testQuery']);
 Route::post('login', [AirPlaneFlightControllerAPI::class, 'login']);
 Route::post('register', [AirPlaneFlightControllerAPI::class, 'register']);
+
+//POSTS
+Route::get('get-all-posts', [PostController::class, 'getAllPosts']);
+Route::get('get-post', [PostController::class, 'getPost']);
+Route::get('search-post', [PostController::class, 'searchPost']);
